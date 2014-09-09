6e5495a251cc5d51e972783e37129d1cbce0637d
karun012 <karun012@gmail.com>
2014-09-09 10:06:18 -0700

    Adding doctest-discover driver

--- test/doctests.hs
+++ test/doctests.hs
@@ -1,14 +1,1 @@
-import Test.DocTest
-import System.Directory
-import Data.List
-import System.FilePath.Posix
-
-main :: IO ()
-main = getSourceFiles >>= \sourceFiles -> doctest $
-      "-isrc" : sourceFiles
-
-getSourceFiles :: IO [FilePath]
-getSourceFiles = do 
-                files <- getDirectoryContents "src"
-                let haskellSourceFiles = (map dropExtension . filter (`notElem` [".", ".."]) . filter (isSuffixOf ".hs")) files
-                return haskellSourceFiles
+{-# OPTIONS_GHC -F -pgmF doctest-discover #-}
