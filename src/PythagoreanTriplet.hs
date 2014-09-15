7ccb331fd3e9b7d0afa27eb12d944a0f87cdb130
karun012 <karun012@gmail.com>
2014-09-15 15:09:38 -0700

    Improved version

--- src/PythagoreanTriplet.hs
+++ src/PythagoreanTriplet.hs
@@ -1,7 +1,18 @@
 module PythagoreanTriplet where
 
--- | Special triplet where a + b + c = 1000
---
-specialTriplet :: (Integer, Integer, Integer)
-specialTriplet = head $ dropWhile (\(a,b,c) -> a + b + c /= 1000) $ [(a,b,c) | c <- [1..], b <- [1..c], a <- [1..b], a^2 + b^2 == c^2]
+import Data.List
 
+-- | Generating triplets with the required conditions using Euclid's formula
+--
+-- Given two integers m and n with m > n
+-- A pythagorean triplet (a,b,c) can be defined in terms of m and n as 
+-- a = m^2 - n^2, b = 2 * m * n, c = m^2 + n^2
+--
+specialTriplet :: Maybe (Integer,Integer,Integer)
+specialTriplet = find (\(a,b,c) -> a + b + c == 1000) (triplets combinations)
+
+triplets :: [(Integer,Integer)] -> [(Integer,Integer,Integer)]
+triplets mnCombinations = map (\(m,n) -> (m^2 - n^2, 2 * m * n, m^2 + n^2)) mnCombinations
+
+combinations :: [(Integer,Integer)]
+combinations = [(m,n) | m <- [1..], n <- [1..m]]
