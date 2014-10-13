8046a08f275ba4666c54393be81201f2c2f81af2
karun012 <karun012@gmail.com>
2014-10-13 10:32:14 -0700

    Improved performance of largest collatz sequence by using maximumBy instead of sorting and getting the first element. sortBy uses merge sort. maximumBy is a fold over the list. So maximumBy should be much faster

--- src/CollatzSequence.hs
+++ src/CollatzSequence.hs
@@ -13,7 +13,7 @@ main = do
       let max = 1000000
       let blocks = chunksOf blockSize [min..max]
       results <- parallel (map largestCollatzSequenceInBlock blocks)
-      let maxLengthCollatzSequence = maximumBy (\(_, x) (_, y) -> if x > y then GT else LT) results
+      let maxLengthCollatzSequence = largestChain results
       print $ "Max length collatz sequence between " ++ show min ++ " and " ++ show max ++ ": " ++ show maxLengthCollatzSequence
       stopGlobalPool
 
@@ -21,9 +21,12 @@ largestCollatzSequenceInBlock :: [Integer] -> IO (Integer,Integer)
 largestCollatzSequenceInBlock xs = do
                                 let start = head xs
                                 let chains = collatzChains xs
-                                let largestCollatzSequence = (head . sortCollatzSequences) chains
+                                let largestCollatzSequence = largestChain chains
                                 print $ "Largest sequence in block starting at " ++ show start ++ " : " ++ show (largestCollatzSequence) ++ "\n"
                                 return (largestCollatzSequence)
+
+largestChain :: [(Integer, Integer)] -> (Integer, Integer)
+largestChain xs = maximumBy (\(_, x) (_, y) -> if x > y then GT else LT) xs
 
 collatz n
       | even n = n `div` 2
@@ -33,10 +36,8 @@ collatzChain :: Integer -> [Integer]
 collatzChain 1 = [1]
 collatzChain n = collatz n : collatzChain (collatz n)
 
-sortCollatzSequences :: [(Integer, Integer)] -> [(Integer, Integer)]
-sortCollatzSequences xs = sortBy (\(_,x) (_,y) -> if x < y then GT else LT) xs
-
 collatzChains :: [Integer] -> [(Integer, Integer)]
-collatzChains xs = map (\x -> (x, (toInteger . (+1) . length . takeWhile (/=1) . iterate collatz) x)) xs
--- collatzChains xs = map (\x -> (x, (toInteger . length . takeWhile (/=1) . collatzChain) x)) xs
+collatzChains xs = map (\x -> (x, (toInteger . accountForMissingElementFromSequence . length . takeWhile (/=1) . iterate collatz) x)) xs
 
+accountForMissingElementFromSequence :: Int -> Int
+accountForMissingElementFromSequence = (+1)
